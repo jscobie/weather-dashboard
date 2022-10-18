7514d4bc76a93c005fdfcc4fdb223176c579b7b3
Jacob Scobie <jscobie@focus-solutions.net>
2022-10-17 22:13:52 -0400

    work with instructor on boxes and psuedo

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var apiLink = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
+// pull in typed city name
+// translate city name to latitude and longitude
+    // get request or api
+// fetch request to get current date forecast data
+// 
